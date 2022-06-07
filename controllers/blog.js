5a824eef533d82c315525a185117513887e25aaf
FuFuShIh <zx8714245@gmail.com>
2022-06-07 14:21:19 +0800

    Fixed the upload image problem

--- controllers/blog.js
+++ controllers/blog.js
@@ -66,8 +66,8 @@ exports.create = (req, res)=> {
         })
       }
 
-      blog.photo.data = fs.readFileSync(files.photo.filepath, 'utf8')
-      blog.photo.contentType = files.photo.type
+      blog.photo.data = fs.readFileSync(files.photo.filepath)
+      blog.photo.contentType = files.photo.mimetype
     }
 
     blog.save((err, result) => {
@@ -101,3 +101,189 @@ exports.create = (req, res)=> {
     })
   })
 }
+
+exports.list = (req, res) => {
+  Blog.find({})
+    .populate('categories', '_id name slug')
+    .populate('tags', '_id name slug')
+    .populate('postedBy', '_id name username')
+    .select('_id title slug excerpt categories tags postedBy createdAt updateAt')
+    .exec((err, data)=> {
+      if(err) {
+        return res.json({
+          error: errorHandler(err)
+        })
+      }
+      res.json({
+        blogs: data
+      })
+    })
+}
+
+exports.listAllBlogsCategoriesTags = (req, res) => {
+  let limit = req.body.limit ? parseInt(req.body.limit) : 10
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0
+
+  let blogs
+  let categories
+  let tags
+
+  Blog.find({})
+    .populate('categories', '_id name slug')
+    .populate('tags', '_id name slug')
+    .populate('postedBy', '_id name username')
+    .sort({createdAt: -1})
+    .skip(skip)
+    .limit(limit)
+    .select('_id title slug excerpt categories tags postedBy createdAt updateAt')
+    .exec((err, data)=> {
+      if(err) {
+        return res.json({
+          error: errorHandler(err)
+        })
+      }
+      
+      blogs = data
+      // get all categories
+      Category.find({}).exec((err, c)=> {
+        if(err) {
+          return res.json({
+            error: errorHandler(err)
+          })
+        }
+        categories = c // categories
+
+        // get all tags
+        Tag.find({}).exec((err, t)=> {
+          if(err) {
+            return res.json({
+              error: errorHandler(err)
+            })
+          }
+          tags = t //tags
+          // return all
+          res.json({
+            blogs,
+            categories,
+            tags,
+            size: blogs.length
+          })
+        })
+      })
+    })
+}
+
+exports.read = (req, res) => {
+  const slug = req.params.slug.toLowerCase()
+  Blog.findOne({slug})
+    .populate('categories', '_id name slug')
+    .populate('tags', '_id name slug')
+    .populate('postedBy', '_id name username')
+    .select('_id title slug mtitle mdesc excerpt categories tags postedBy createdAt updateAt')
+    .exec((err, data) => {
+      if(err) {
+        return res.json({
+          error: errorHandler(err)
+        })
+      }
+
+      res.json(data)
+    })
+}
+
+exports.remove = (req, res) => {
+  const slug = req.params.slug.toLowerCase()
+  Blog.findOneAndRemove({slug})
+    .exec((err, data) => {
+      if(err) {
+        return res.json({
+          error: errorHandler(err)
+        })
+      }
+
+      res.json({
+        message: 'Blog deleted successfully'
+      })
+    })
+}
+
+exports.update  = (req, res)=> {
+  const slug = req.params.slug.toLowerCase();
+
+  Blog.findOne({slug}).exec((err, oldBlog) => {
+    if(err) {
+      return res.json({
+        error: errorHandler(err)
+      })
+    }
+
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, files) => {
+      if(err) {
+        return res.status(400).json({
+          error: 'Image could not upload'
+        })
+      }
+  
+      let slugBeforeMerge = oldBlog.slug
+      oldBlog = _.merge(oldBlog, fields)
+      oldBlog.slug = slugBeforeMerge;
+
+      const {body, mdesc, categories, tags} = fields
+
+      if (body) {
+          oldBlog.excerpt = smartTrim(body, 320, ' ', ' ...');
+          oldBlog.mdesc = stripHtml(body.substring(0, 160));
+      }
+
+      if (categories) {
+          oldBlog.categories = categories.split(',');
+      }
+
+      if (tags) {
+          oldBlog.tags = tags.split(',');
+      }      
+  
+      if(files.photo) {
+        if(files.photo.size > 1000000) {
+          return res.status(400).json({
+            error: 'Image should be less then 1mb in size'
+          })
+        }
+  
+        oldBlog.photo.data = fs.readFileSync(files.photo.filepath, 'utf8')
+        oldBlog.photo.contentType = files.photo.type
+      }
+  
+      oldBlog.save((err, result) => {
+        if(err) {
+          // console.log(err)
+          return res.status(400).json({
+            error: errorHandler(err)
+          })
+        }
+        // result.photo=undefined
+        res.json(result)
+      })
+    })
+  })
+
+}
+
+exports.photo = (req, res) => {
+  const slug = req.params.slug.toLowerCase();
+  Blog.findOne({ slug })
+      .select('photo')
+      .exec((err, blog) => {
+          if (err || !blog) {
+              return res.status(400).json({
+                  error: errorHandler(err)
+              });
+          }
+          // console.log(blog.photo.data)
+          res.set('Content-Type', blog.photo.contentType);
+          return res.send(blog.photo.data);
+      });
+};
