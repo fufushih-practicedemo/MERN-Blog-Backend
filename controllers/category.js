88f5cec0dc39fbff86522be53e0ee9b7af69b1f8
FuFuShIh <zx8714245@gmail.com>
2022-06-08 10:50:17 +0800

    Add category and tag to find out blog post

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,7 @@
 const { default: slugify } = require('slugify')
 const Category = require('../models/category')
 const { errorHandler } = require('../helpers/dbErrorHandler')
+const Blog = require('../models/blog')
 
 exports.create = (req, res) => {
   const {name} = req.body
@@ -40,7 +41,20 @@ exports.read = (req, res) => {
       })
     }
 
-    res.json(category); //
+    // res.json(category); //
+    Blog.find({categories: category})
+      .populate('categories', '_id name slug')
+      .populate('tags', '_id name slug')
+      .populate('postedBy', '_id name')
+      .select('_id title slug excerpt categories postedBy tags createdAt updatedAt')
+      .exec((err, data)=> {
+        if(err) {
+          return res.json(400).json({
+            error: errorHandler(err)
+          })
+        }
+        res.json({category: category, blogs: data})
+      })
   })
 }
 
--- controllers/tag.js
+++ controllers/tag.js
@@ -1,6 +1,7 @@
 const { default: slugify } = require('slugify');
 const Tag = require('../models/tag');
-const { errorHandler } = require('../helpers/dbErrorHandler')
+const { errorHandler } = require('../helpers/dbErrorHandler');
+const Blog = require('../models/blog');
 
 exports.create = (req, res) => {
   const {name} = req.body
@@ -40,7 +41,20 @@ exports.read = (req, res) => {
       })
     }
 
-    res.json(tag); //
+    // res.json(tag); //
+    Blog.find({ tags: tag })
+      .populate('categories', '_id name slug')
+      .populate('tags', '_id name slug')
+      .populate('postedBy', '_id name')
+      .select('_id title slug excerpt categories postedBy tags createdAt updatedAt')
+      .exec((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        }
+        res.json({ tag: tag, blogs: data });
+      });
   })
 }
 
