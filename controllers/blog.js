9e7820fa6575d52abe5716205fb1fffcdbf02521
FuFuShIh <zx8714245@gmail.com>
2022-06-09 00:17:56 +0800

    Add username prop in list related

--- controllers/blog.js
+++ controllers/blog.js
@@ -292,7 +292,7 @@ exports.listRealate = (req, res) => {
 
   Blog.find({_id: {$ne: _id}, categories: {$in: categories}})
     .limit(limit)
-    .populate('postedBy', '_id name profile')
+    .populate('postedBy', '_id name username profile')
     .select('title slug excerpt postedBy createdAt updatedAt')
     .exec((err, blogs)=> {
       if(err) {
