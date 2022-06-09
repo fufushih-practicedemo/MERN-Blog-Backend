b915123ddc87fef135136265016be79d33a2155e
FuFuShIh <zx8714245@gmail.com>
2022-06-09 13:02:25 +0800

    Add the api with creating blog by authenticated user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 const User = require('../models/user')
+const Blog = require('../models/blog')
 const shortId = require('shortid')
 const jwt = require('jsonwebtoken')
 const {expressjwt} = require('express-jwt')
+const {errorHandler} = require('../helpers/dbErrorHandler')
 
 exports.signup = (req, res) => {
   User.findOne({email: req.body.email}).exec((err, user) => {
@@ -108,3 +110,21 @@ exports.adminMiddleware = (req, res, next) => {
     next()
   })
 }
+
+exports.canUpdateDeleteBlog = (req, res, next) => {
+  const slug = req.params.slug.toLowerCase();
+  Blog.findOne({ slug }).exec((err, data) => {
+      if (err) {
+          return res.status(400).json({
+              error: errorHandler(err)
+          });
+      }
+      let authorizedUser = data.postedBy._id.toString() === req.profile._id.toString();
+      if (!authorizedUser) {
+          return res.status(400).json({
+              error: 'You are not authorized'
+          });
+      }
+      next();
+  });
+};
